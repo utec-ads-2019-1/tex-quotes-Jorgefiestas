1cfe0d1413d778127b89d02a7ac4b6f2742bdbef
Jorge Ignacio Fiestas Lavezzolo <jorge@amigocloud.com>
2019-03-29 15:48:50 -0500

    Improves code legibility

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+string line;
+bool open = true;
+
+void process_line(string &, string &);
 
 int main(int argc, char *argv[]) {
-	string line;
-	bool open = true;
-	while(getline(cin, line)){
-		string res = "";
-		for(int i = 0; i< line.size(); i++){
-			if(line[i] == '"'){
-				if(open) res += "``";
-				else res += "''";
-				open = !open;
-			} else res += line[i];
-		}
-		cout<<res<<endl;
-	}
+	string modified_string = "";
+	while(getline(cin, line))
+		process_line(line, modified_string);
+	cout<<modified_string<<endl;
     return 0;
+}
+
+void process_line(string &in, string &out){
+	for(int i = 0; i< in.size(); i++){
+		if(in[i] == '"'){
+			if(open) out += "``";
+			else out += "''";
+			open = !open;
+		}
+		else
+			out += line[i];
+	}
+	out += '\n';
 }
