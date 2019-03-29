b7b61ea18a966f09c2fdef68c221fab9e4d22a79
Jorge Ignacio Fiestas Lavezzolo <jorge@amigocloud.com>
2019-03-28 21:17:48 -0500

    Adds Solution

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	string line;
+	bool open = true;
+	while(getline(cin, line)){
+		string res = "";
+		for(int i = 0; i< line.size(); i++){
+			if(line[i] == '"'){
+				if(open) res += "``";
+				else res += "''";
+				open = !open;
+			} else res += line[i];
+		}
+		cout<<res<<endl;
+	}
     return 0;
 }
